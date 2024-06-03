b94711f066c35c336bcf22370d52c8728e41db26
Ian Lima <ianbernardolim@gmail.com>
2024-06-03 13:49:05 -0300

    Introdução ao EF e ao Angular

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public EventosController(DataContext dataContext){
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _dataContext.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _dataContext.Eventos.Find(id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+           return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+           return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id){
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
